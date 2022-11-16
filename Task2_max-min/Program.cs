5ba4976228001f63d6ffbef001ee1ec72130a648
idoctorvic <loukianova@yahoo.com>
2022-11-16 16:13:51 +0300

    add Task2 Maximum of two

---
+++ Task2_max-min/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+Console.Write("Enter the first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+if (num1 > num2) 
+{
+    Console.WriteLine($"Number {num1} is the maximum of the two numbers");
+}
+else 
+{
+    Console.WriteLine($"Number {num2} is the maximum of the two numbers");
+}
