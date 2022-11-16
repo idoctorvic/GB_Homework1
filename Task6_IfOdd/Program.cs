fa76a4338e8f84e00f7128c6ad8ddc9e61658566
idoctorvic <loukianova@yahoo.com>
2022-11-16 16:32:30 +0300

    add Task6 If number is even or odd

---
+++ Task6_IfOdd/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+Console.Write("Enter a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num % 2 == 0)
+{
+    Console.WriteLine("The number is even");
+}
+else
+{
+    Console.WriteLine("The number is odd");
+}
